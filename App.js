a39c75a8e929abc4d694017a6fdb2b24771c16b4
Romina Manzano <rominam@nimble.la>
2023-06-08 09:25:21 -0300

    UPDATE: start screen styles

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 const App = () => (
-  <StartGameScreen />
+  <LinearGradient
+    style={styles.rootScreen}
+    colors={['#4E0329', '#DDB52F']}
+  >
+    <ImageBackground
+      source={require('./assets/images/background.png')}
+      resizeMode="cover"
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImage}
+    >
+      <StartGameScreen />
+    </ImageBackground>
+  </LinearGradient>
 );
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
